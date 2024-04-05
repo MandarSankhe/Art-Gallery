0ef3f2721211f2c00959ba36abe98c16f6c376a0
MandarSankhe <45758847+MandarSankhe@users.noreply.github.com>
2024-04-05 18:15:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(() => {
+    const myCarouselElement = document.querySelector('#carouselExampleDark')
+
+    const carousel1 = new bootstrap.Carousel(myCarouselElement, {
+        interval: 2200
+    })
+
+});
